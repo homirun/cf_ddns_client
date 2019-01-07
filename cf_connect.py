4968fa538496986a691f251c6819c57484c5c30f
homirun <hatunemiku.moco@gmail.com>
2019-01-08 04:18:27 +0900

    record idの取得の実装

--- cf_connect.py
+++ cf_connect.py
@@ -4,14 +4,15 @@ import json
 
 class CFConnect:
 
-    def __init__(self, domain_name, email, API_KEY, END_POINT_BASE_URL):
+    def __init__(self, domain_name, domain_record, email, API_KEY, END_POINT_BASE_URL):
         self.API_KEY = API_KEY
         self.END_POINT_BASE_URL = END_POINT_BASE_URL
         self.domain_name = domain_name
+        self.domain_record = domain_record
         self.email = email
 
     def set_ip(self):
-        res = requests.put()
+        pass
 
     def _get_zone_id(self):
         url = self.END_POINT_BASE_URL + "zones?name=" + self.domain_name + "&status=active&page=1&per_page=20" \
@@ -22,4 +23,8 @@ class CFConnect:
         return zone_id
 
     def get_record_id(self):
-        url = self.END_POINT_BASE_URL + "zones/" + self._get_zone_id() + ""
+        url = self.END_POINT_BASE_URL + "zones/" + self._get_zone_id() + "/dns_records?name=" + self.domain_record
+        headers = {'X-Auth-Email': self.email, 'X-Auth-Key': self.API_KEY, 'Content-Type': "application/json"}
+        res = requests.get(url, headers=headers)
+        record_id = json.loads(res.content)["result"][0]["id"]
+        return record_id
--- main.py
+++ main.py
@@ -7,28 +7,29 @@ def main():
     API_KEY = ""
     END_POINT_BASE_URL = "https://api.cloudflare.com/client/v4/"
     domain_name = ""
+    domain_record = ""
     email = ""
 
     # debug use only!
     def debug_init():
         with open("./debug_config") as f:
             l = [s.strip() for s in f.readlines()]
-            nonlocal API_KEY, domain_name, email
-            API_KEY= l[0]
+            nonlocal API_KEY, domain_name, email, domain_record
+            API_KEY = l[0]
             domain_name = l[1]
-            email = l[2]
+            domain_record = l[2]
+            email = l[3]
 
     debug_init()
 
     ip = get_ip()
-    connector = CFConnect(domain_name, email, API_KEY, END_POINT_BASE_URL)
-    connector._get_zone_id()
+    connector = CFConnect(domain_name, domain_record, email, API_KEY, END_POINT_BASE_URL)
+    connector.get_record_id()
 
 
 def get_ip():
     res = requests.get('http://api.ipify.org/')
     return res
-
 
 
 if __name__ == '__main__':
